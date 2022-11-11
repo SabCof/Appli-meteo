9924a99c9b34e6391bd7db2cdd8d79fd4e7b7799
Sabrina Cofflard <65305870+SabCof@users.noreply.github.com>
2022-11-11 16:56:40 +0100

    reste à modif la ville, les icons et faire un Math.ceil

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,57 @@
-/* Appel a l'API openweather avec ville et système métrique en paramètre de fonction*/
-const apiKey= "3ad0baf152741088efafc8b1ce9fbaf8",
-    
- apiCall = function (city){
-    const url ='https://api.openweathermap.org/data/2.5/weather?q='+ city + '&lang=fr&units=metric&appid='+ apiKey; 
-      
-     
-     
-     
-     fetch(url)
+let ville = 'Saint-Astier';
+// Elements
+const city = document.querySelector('#ville');
+const main = document.querySelector('#weatherMain');
+const icons = document.querySelector('#weatherIcone');
+const description = document.querySelector('#weatherDescp');
+const temperature = document.querySelector("#temperatureLabel");
+const humidity = document.querySelector("#humidityLabel");
+const wind = document.querySelector("#windLabel");
+const appid = '3ad0baf152741088efafc8b1ce9fbaf8';
+const btnCity = document.querySelector("#changer");
+
+//Afficher à l'entrée de la page la météo d'une ville par défault, ici St Astier
+// Pour ça on appelle la fonction recevoirMeteo();
+
+recevoirMeteo(ville);
+function recevoirMeteo(city) {
+   
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&units=metric&appid=${appid}`;
+    const imgSrc = `http://openweathermap.org/img/wn/${icons}@2x.png`;
+
+    fetch(url)
         .then((response) => 
-            response.json().then((data) => {
-                console.log(data);
-        
-                document.querySelector('#city').innerHTML = data.name;
-                document.querySelector("#temp").innerHTML =  data.main.temp + '°';
-                document.querySelector("#humidity").innerHTML =  data.main.humidity + '%';
-                document.querySelector("#wind").innerHTML =  data.wind.speed + 'km/h';
+            response.json()
+            .then((data) => {
+                
+                ville = data.name;
+                main.innerHTML = data.weather[0].main;
+                icons.innerHTML = data.weather[0].icon;
+                let img = document.createElement('img');
+                img.src.innerHTML = imgSrc;
+                description.innerHTML = data.weather[0].description;
+                temperature.innerHTML =  data.main.temp + '°C';
+                humidity.innerHTML =  data.main.humidity + '%';
+                wind.innerHTML =  data.wind.speed + 'km/h';
+     
             })    
-        ) 
+        ); 
+}
+
+btnCity.addEventListener('click', () =>{
+    let villeChoisie = ville ;
+    villeChoisie = prompt('Quelle ville souhaitez-vous choisir ?');
+    recevoirMeteo(villeChoisie);
+})
+
+     
+     
+     
           
 
 
-};
 
-/* Ecouteur d'Event sur la soumission du formulaire */
 
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    let ville = document.querySelector('#inputCity').value;
-    apiCall(ville);
-});
-apiCall('perigueux');
 
 
 
